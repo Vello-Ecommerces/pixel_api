164ea9bcc3fa97be356e94cd11978695bb55e1eb
Ederson Vinicius <ederson@grupovello.com.br>
2025-10-01 10:30:27 -0300

    allowed preflight

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ dotenv.config();
 const { Pool } = pkg;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
