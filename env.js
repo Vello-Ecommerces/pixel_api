062137f2005437e917ac6649e954c87fdddb8ae5
Ederson Vinicius <ederson@grupovello.com.br>
2025-10-01 13:34:26 -0300

    type changes

--- env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const HOST = process.env.HOST;
-export const PORT = process.env.PORT;
-export const DATABASE = process.env.DATABASE;
-export const USER = process.env.USER;
-export const PASSWORD = process.env.PASSWORD;
-export const DATABASE_URL = process.env.DATABASE_URL;
